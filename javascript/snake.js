a95e067d2a7e8af46fccdd3b9bc4223867f59cee
Walter T. Bormans <bormanwa@ie.ibm.com>
2017-05-24 16:43:59 +0100

    Adds background object in snake.js

--- javascript/snake.js
+++ javascript/snake.js
@@ -1,5 +1,42 @@
 var imageRepository = new function() {
 	this.background = new Image();
 
-	this.background.src = "images/background.png";
+	this.background.src = "../images/background.png";
 }
+
+
+/**
+ * Creates the Drawable object, which will serve as the base
+ * class for all drawable objects in the game.
+ */
+
+function Drawable() {
+	this.init = function(x, y){
+		this.x = x;
+		this.y = y;
+	}
+	this.speed = 0;
+	this.canvasWidth=0;
+	this.canvasHeight=0;
+
+	this.draw = function() {};
+}
+
+
+// Child of Drawable. This will pan, thus creating the illusion of movement
+function Background() {
+	this.speed = 1;
+
+	this.draw = function() {  // implement the earlier absctract function
+		this.y += this.speed;
+
+		this.context.drawImage(imageRepository.background, this.x this.y);
+		this.context.drawImage(imageRepository.background, this.x this.y - this.canvasHeight);
+		if (this.y >= this.canvasHeight){
+			this.y = 0;
+		};
+	}
+
+}
+Background.prototype = new Drawable();
+
