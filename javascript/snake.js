0acdd0f8d97863a3d0315a992c53e3b14dfcfa60
Walter T. Bormans <bormanwa@ie.ibm.com>
2017-05-25 11:04:34 +0100

    Adds movement/scrolling background

--- javascript/snake.js
+++ javascript/snake.js
@@ -40,3 +40,32 @@ function Background() {
 }
 Background.prototype = new Drawable();
 
+
+
+
+function animate() {
+	requestAnimFrame(animate);
+	game.background.draw();
+}
+/**
+ * requestAnim shim layer by Paul Irish
+ * Finds the first API that works to optimize the animation loop,
+ * otherwise defaults to setTimeout().
+ */
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(/* function */ callback, /* DOMElement */ element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
+
+var game = new Game();
+
+function init(){
+	if(game.init())
+		game.start();
+}
